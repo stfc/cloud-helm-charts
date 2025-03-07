7e22c64cebe1e670ebf7b2dc9d47de054cdfd3c4
anish-mudaraddi <anish.mudaraddi@stfc.ac.uk>
2025-03-07 13:51:11 +0000

    allow installing helm chart directly
    various changes to allow users to install helm chart directly using helm install without needing to pull the github repo down.

--- charts/stfc-cloud-openstack-cluster/scripts/bootstrap.sh
+++ charts/stfc-cloud-openstack-cluster/scripts/bootstrap.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# Check if correct number of arguments provided
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <ip-address>"
-    exit 1
-fi
-
 # Function to convert dependencies to a valid environment variables
 sanitize_var_name() {
     echo "$1" | tr '-' '_' | tr '[:lower:]' '[:upper:]'
@@ -36,14 +30,6 @@ set_env_vars() {
 # Set environment variables from dependencies.json
 set_env_vars "dependencies.json"
 
-IP_ADDRESS=$1
-
-# Check a clouds.yaml filepath exists (by default checks if in same directory)
-if [ ! -f "clouds.yaml" ]; then
-    echo "Error: clouds.yaml file does not exist"
-    exit 1
-fi
-
 echo "Updating system to apply latest security patches..."
 export DEBIAN_FRONTEND=noninteractive
 sudo apt-get update -qq
@@ -51,43 +37,14 @@ sudo apt-get update -qq
 sudo apt-get upgrade -y -qq > /dev/null
 
 echo "Installing required tools..."
-sudo apt-get install -y snapd python3-openstackclient
+sudo apt-get install -y snapd
 export PATH=$PATH:/snap/bin
 sudo snap install kubectl --classic
 sudo snap install helm --classic
-sudo snap install yq
 
 curl --no-progress-meter -L "https://github.com/kubernetes-sigs/cluster-api/releases/download/${CLUSTER_CTL_VERSION}/clusterctl-linux-amd64" -o clusterctl
 chmod +x clusterctl
 sudo mv clusterctl /usr/local/bin/clusterctl
-
-# Check that application_credential_id existing in clouds.yaml
-# This has to be done after yq is installed
-if [ "$(yq -r '.clouds.openstack.auth.application_credential_id' clouds.yaml)" == "null" ]; then
-    # Enforce the use of app creds
-    echo "Error: An app cred clouds.yaml file is required in the clouds.yaml file, normal creds (i.e. those with passwords) are not supported"
-    exit 1
-fi
-
-if [ "$(yq -r '.clouds.openstack.auth.project_id' clouds.yaml)" == "null" ]; then
-    echo "Looking up project_id for clouds.yaml..."
-    APP_CRED_ID=$(yq -r '.clouds.openstack.auth.application_credential_id' clouds.yaml)
-    PROJECT_ID=$(openstack --os-cloud openstack application credential show "${APP_CRED_ID}" -c project_id -f value)
-    echo "Injecting project ID: '${PROJECT_ID}' into clouds.yaml..."
-    injected_id=$PROJECT_ID yq e '.clouds.openstack.auth.project_id = env(injected_id)' -i clouds.yaml
-fi
-
-# Setup Secrets file
-# Read the entire clouds section from the credentials file and combine with IP
-mkdir -p /tmp/capi
-cat > /tmp/capi/secret-values.yaml << EOF
-openstack-cluster:
-  apiServer: 
-    floatingIP: $IP_ADDRESS
-$(grep -v '^#' clouds.yaml | yq '.' | sed 's/^/  /')
-EOF
-echo "created secrets file in /tmp/capi/secret-values.yaml"
-
 
 echo "Installing and starting microk8s..."
 sudo snap install microk8s --classic
@@ -102,7 +59,6 @@ sudo chown "$USER" ~/.kube/config
 sudo chmod 600 ~/.kube/config
 sudo microk8s enable dns
 
-
 echo "Initialising cluster-api OpenStack provider..."
 echo "If this fails you may need a GITHUB_TOKEN, see https://stfc.atlassian.net/wiki/spaces/CLOUDKB/pages/211878034/Cluster+API+Setup for details"
 clusterctl init --infrastructure=openstack:"${CAPO_PROVIDER_VERSION}"
@@ -116,4 +72,3 @@ helm upgrade cluster-api-addon-provider capi-addons/cluster-api-addon-provider -
 
 echo "You are now ready to create a cluster following the remaining instructions..."
 echo "https://stfc.atlassian.net/wiki/spaces/CLOUDKB/pages/211878034/Cluster+API+Setup"
-
